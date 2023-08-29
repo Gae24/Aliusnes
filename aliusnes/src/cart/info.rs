39524ca31b7a86d9cb4f874b93908c96846b2fb8
Gae24 <96017547+Gae24@users.noreply.github.com>
2023-08-29 12:23:00 +0200

    basic cartridge recognition

---
+++ aliusnes/src/cart/info.rs
@@ -0,0 +1,46 @@
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum Mapper {
+    LoROM,
+    HiROM,
+    SA1ROM,
+    SDD1ROM,
+    ExHiROM,
+}
+
+impl Mapper {
+    pub fn get_base_mapper(self) -> Self {
+        match self {
+            Self::LoROM | Self::SA1ROM | Self::SDD1ROM => Self::LoROM,
+            Self::HiROM => Self::HiROM,
+            Self::ExHiROM => Self::ExHiROM,
+        }
+    }
+}
+
+pub struct Chipset {
+    pub has_coprocessor: bool,
+    pub has_ram: bool,
+    pub has_battery: bool,
+}
+
+pub enum Region {
+    Japan,
+    NorthAmerica,
+    Europe,
+    Sweden,
+    Finland,
+    Denmark,
+    France,
+    Netherlands,
+    Spain,
+    Germany,
+    Italy,
+    China,
+    Indonesia,
+    SouthKorea,
+    International,
+    Canada,
+    Brazil,
+    Australia,
+    Unknown(u8),
+}
