843aa3d9d5688b56494b2b9997cd7012a512100c
Gae24 <96017547+Gae24@users.noreply.github.com>
2023-09-06 19:51:15 +0200

    added wram

--- aliusnes/src/bus/bus.rs
+++ aliusnes/src/bus/bus.rs
@@ -1,25 +1,29 @@
-use crate::cart::cart::Cart;
+use crate::{cart::cart::Cart, wram::Wram};
 
 pub struct Bus {
+    wram: Wram,
     cart: Cart,
 }
 
 impl Bus {
     pub fn new(cart: Cart) -> Self {
-        Self { cart }
+        Self {
+            cart,
+            wram: Wram::new(),
+        }
     }
 
     pub fn read(&self, addr: u32) -> u8 {
         let bank = (addr >> 16) as u8;
         match bank {
             0x00..=0x3F | 0x80..=0xBF => match (addr >> 8) as u8 {
-                0x00..=0x1F => todo!(),
+                0x00..=0x1F => return self.wram.ram[addr as usize & 0x1FFF],
                 0x21 => return self.read_mmio(addr as u16),
                 0x40..=0x43 => return self.read_mmio(addr as u16),
                 _ => {}
             },
 
-            0x7E..=0x7F => todo!(),
+            0x7E..=0x7F => return self.wram.ram[addr as usize & 0x1_FFFF],
             _ => {}
         }
         return self.cart.read(bank, addr as u16);
@@ -29,13 +33,13 @@ impl Bus {
         let bank = (addr >> 16) as u8;
         match bank {
             0x00..=0x3F | 0x80..=0xBF => match (addr >> 8) as u8 {
-                0x00..=0x1F => todo!(),
+                0x00..=0x1F => return self.wram.ram[addr as usize & 0x1FFF] = data,
                 0x21 => return self.write_mmio(addr as u16, data),
                 0x40..=0x43 => return self.write_mmio(addr as u16, data),
                 _ => {}
             },
 
-            0x7E..=0x7F => todo!(),
+            0x7E..=0x7F => return self.wram.ram[addr as usize & 0x1_FFFF] = data,
             _ => {}
         }
         self.cart.write(bank, addr as u16, data);
