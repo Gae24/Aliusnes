48c34c3c1190ec9f439858492c5af33de3bbd3b2
Gae24 <96017547+Gae24@users.noreply.github.com>
2023-12-24 11:55:34 +0100

    added some derive and new method for Dma

--- aliusnes/src/bus/dma.rs
+++ aliusnes/src/bus/dma.rs
@@ -1,6 +1,7 @@
 use proc_bitfield::bitfield;
 
 bitfield! {
+    #[derive(Copy, Clone)]
     pub struct Configuration(u8) {
         pub transfer_pattern: u8 @ 0..=2,
         pub address_adjust_mode: u8 @ 3..=4,
@@ -9,6 +10,7 @@ bitfield! {
     }
 }
 
+#[derive(Copy, Clone)]
 pub struct Channel {
     configuration: Configuration,
     b_addr: u8,
@@ -24,4 +26,23 @@ pub struct Dma {
     channels: [Channel; 8],
     enable_channels: u8,
     h_enable_channels: u8,
+}
+
+impl Dma {
+    pub fn new() -> Self {
+        Dma {
+            channels: [Channel {
+                configuration: Configuration(0),
+                b_addr: 0,
+                a_addr_or_h_table_addr: 0,
+                a_bank_or_h_table_bank: 0,
+                byte_count_or_h_indirect_addr: 0,
+                h_indirect_bank: 0,
+                h_curr_addr: 0,
+                unused: 0,
+            }; 8],
+            enable_channels: 0,
+            h_enable_channels: 0,
+        }
+    }
 }
