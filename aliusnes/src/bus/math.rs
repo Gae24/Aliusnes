1445cc42286199f2385ff07863b0d317d70761d4
Gae24 <96017547+Gae24@users.noreply.github.com>
2024-01-01 13:46:52 +0100

    Implemented Access trait for Math struct

--- aliusnes/src/bus/math.rs
+++ aliusnes/src/bus/math.rs
@@ -1,3 +1,6 @@
+use super::access::Access;
+use crate::utils::int_traits::ManipulateU16;
+
 // In hardware multiplication takes 8 cycles,
 // while division 16
 pub struct Math {
@@ -21,17 +24,46 @@ impl Math {
         }
     }
 
-    pub fn do_multiplication(&mut self) {
+    fn do_multiplication(&mut self) {
         self.result_or_remainder = self.factor_a as u16 * self.factor_b as u16;
     }
 
-    pub fn do_division(&mut self) {
+    fn do_division(&mut self) {
         if self.divisor == 0 {
             self.quotient = 0xFFFF;
             self.result_or_remainder = self.dividend;
         } else {
             self.quotient = self.dividend / self.divisor as u16;
             self.result_or_remainder = self.dividend % self.divisor as u16;
+        }
+    }
+}
+
+impl Access for Math {
+    fn read(&self, addr: u16) -> u8 {
+        match addr {
+            0x4214 => self.quotient as u8,
+            0x4215 => (self.quotient >> 8) as u8,
+            0x4216 => self.result_or_remainder as u8,
+            0x4217 => (self.result_or_remainder >> 8) as u8,
+            _ => unreachable!(),
+        }
+    }
+
+    fn write(&mut self, addr: u16, data: u8) {
+        match addr {
+            0x4202 => self.factor_a = data,
+            0x4203 => {
+                self.factor_b = data;
+                self.do_multiplication();
+            }
+            0x4204 => self.dividend.set_low_byte(data),
+            0x4205 => self.dividend.set_high_byte(data),
+            0x4206 => {
+                self.divisor = data;
+                self.do_division();
+            }
+            _ => unreachable!(),
         }
     }
 }
