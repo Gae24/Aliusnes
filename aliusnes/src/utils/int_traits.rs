e867f762ebc5a90d045ffe372e931ed4d34e34d1
Gae24 <96017547+Gae24@users.noreply.github.com>
2024-01-01 11:28:06 +0100

    Added trait to manipulate an u16

--- aliusnes/src/utils/int_traits.rs
+++ aliusnes/src/utils/int_traits.rs
@@ -6,22 +6,18 @@ pub trait ManipulateU16 {
 }
 
 impl ManipulateU16 for u16 {
-    #[inline]
     fn low_byte(self) -> u8 {
         self as u8
     }
 
-    #[inline]
     fn high_byte(self) -> u8 {
         (self >> 8) as u8
     }
 
-    #[inline]
     fn set_low_byte(&mut self, low: u8) {
         *self = (*self & 0xFF00) | (low as u16);
     }
 
-    #[inline]
     fn set_high_byte(&mut self, high: u8) {
         *self = (*self & 0x00FF) | ((high as u16) << 8);
     }
