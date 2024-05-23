5a9525e88bd8605e0945471d73806a2ae7c0187d
Gae24 <96017547+Gae24@users.noreply.github.com>
2024-05-23 10:17:45 +0200

    fix test formatting

--- aliusnes/build.rs
+++ aliusnes/build.rs
@@ -5,14 +5,14 @@ fn main() {
 
     file.write_all("mod tomharte;\n\n".as_bytes())
         .expect("Could not write to file");
-    file.write_all("use tomharte::run_test;\n\n".as_bytes())
+    file.write_all("use tomharte::run_test;\n".as_bytes())
         .expect("Could not write to file");
     for i in 0..256 {
-        let attribute = "#[test]".to_string();
+        let attribute = "\n#[test]".to_string();
         let test_name = format!("test_{:02x}", i);
-        let test_body = format!("run_test(\"{:02x}.n\");", i);
+        let test_body = format!("    run_test(\"{:02x}.n\");", i);
         let test_fn = format!(
-            "{}\npub fn {}() {{\n\t{}\n}}\n\n",
+            "{}\npub fn {}() {{\n{}\n}}\n",
             attribute, test_name, test_body
         );
 
