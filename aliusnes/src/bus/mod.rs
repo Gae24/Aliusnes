e233c34dc73aa3cae8b76e9219241bfaead9e41e
Gae24 <96017547+Gae24@users.noreply.github.com>
2024-06-20 12:49:33 +0200

    Bus trait added methods to check nmi and irq

--- aliusnes/src/bus/mod.rs
+++ aliusnes/src/bus/mod.rs
@@ -10,6 +10,8 @@ pub trait Bus {
     fn read_and_tick(&mut self, addr: Address) -> u8;
     fn write_and_tick(&mut self, addr: Address, data: u8);
     fn add_io_cycles(&mut self, cycles: usize);
+    fn fired_nmi(&self) -> bool;
+    fn fired_irq(&self) -> bool;
 }
 
 pub trait Access {
