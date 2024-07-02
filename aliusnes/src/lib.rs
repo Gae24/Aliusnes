6864c2b7a53b920dfbf78eeaf0c88f019f61a5d3
Gae24 <96017547+Gae24@users.noreply.github.com>
2024-07-02 10:02:20 +0200

    unused function

--- aliusnes/src/lib.rs
+++ aliusnes/src/lib.rs
@@ -2,7 +2,6 @@
 #![feature(generic_const_exprs)]
 
 use cart::{header::Header, Cart};
-use emu::Emu;
 
 pub mod bus;
 pub mod cart;
@@ -13,17 +12,6 @@ pub mod w65c816;
 
 #[macro_use]
 extern crate proc_bitfield;
-
-pub fn run_emu(rom: &[u8], ram: Vec<u8>) {
-    let header = Header::guess_from_rom(rom).expect("Cartridge not recognised");
-    let cart = Cart::new(header, rom, ram);
-
-    let mut emu = Emu::new(cart);
-
-    loop {
-        emu.step();
-    }
-}
 
 pub fn load_cart(rom: &[u8], ram: Vec<u8>) -> Cart {
     let header = Header::guess_from_rom(rom).expect("Cartridge not recognised");
